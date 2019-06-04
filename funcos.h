c1cc098a40b6ccdb5f4204e13bd6243ce350566d
jgallarday <44297397+jgallarday@users.noreply.github.com>
2019-06-04 09:48:57 -0500

    Create funcos.h

---
+++ funcos.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+#ifndef PROYECTO_MARIA_HILDA_MAURIZIO_FUNCOS_H
+#define PROYECTO_MARIA_HILDA_MAURIZIO_FUNCOS_H
+
+
+
+
+
+
+void menu(int & opcion,int & tablero);
+void crea_matriz(vector< vector< int > >& tablero,unsigned int tamano,unsigned int opcion);
+void imprime_matriz(vector< vector< int > >& tablero, unsigned int tamano);
+void mueve_ficha(vector<vector<int>>& tablero, int tamano);
+void detecta_fichas(vector<vector<int>>& tablero, int tamano, bool& running);
+
+
+#endif //PROYECTO_MARIA_HILDA_MAURIZIO_FUNCOS_H
