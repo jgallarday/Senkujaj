e240101c54b748272344b29efe3ae2c7c96dac32
jgallarday <44297397+jgallarday@users.noreply.github.com>
2019-06-04 17:31:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
+
 #include "funcos.h"
 
 
 int main() {
-   int opcion,tamano;
-   bool running=true;
-   menu(opcion,tamano);
-   vector<vector<int>>tablero(tamano, vector<int>(tamano));
-   crea_matriz(tablero,tamano,opcion);
-   while(running) {
-       imprime_matriz(tablero, tamano);
-       mueve_ficha(tablero, tamano);
-       detecta_fichas(tablero, tamano, running);
-   }
-   return 0;
-   }
+    int opcion,tamano;
+    bool game_running=true,running=true;
+    while(game_running) {
+        menu(opcion, tamano,game_running,running);
+        vector<vector<int>> tablero(tamano, vector<int>(tamano));
+        crea_matriz(tablero, tamano, opcion);
+        while (running) {
+            imprime_matriz(tablero, tamano);
+            mueve_ficha(tablero, tamano);
+            detecta_fichas(tablero, tamano, running);
+        }
+    }
+    return 0;
+}
